819f2ea77be512ac6091b65ad1d322c4b90d9620
Simeon Kovachev <skcodingacc@gmail.com>
2023-07-29 18:25:42 +0300

    Combined and modified the Create and Edit actions in the Controller. Made the Layout design. And The Design for the Category Views.

--- Spendopia/Controllers/CategoryController.cs
+++ Spendopia/Controllers/CategoryController.cs
@@ -44,74 +44,32 @@ namespace Spendopia.Controllers
             return View(category);
         }
 
-        // GET: Category/Create
-        public IActionResult Create()
+        // GET: Category/Actions
+        public IActionResult Actions(int id = 0)
         {
-            return View(new Category());
+            if(id == 0)
+                return View(new Category());
+            else
+                return View(_context.Categories.Find(id));
         }
 
-        // POST: Category/Create
+        // POST: Category/Actions
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CategoryId,Title,Icon,Type")] Category category)
+        public async Task<IActionResult> Actions([Bind("CategoryId,Title,Icon,Type")] Category category)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(category);
+                if(category.CategoryId == 0)        
+                    _context.Add(category);
+                else
+                    _context.Update(category);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             return View(category);
         }
 
-        // GET: Category/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories.FindAsync(id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-            return View(category);
-        }
-
-        // POST: Category/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("CategoryId,Title,Icon,Type")] Category category)
-        {
-            if (id != category.CategoryId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(category);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CategoryExists(category.CategoryId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(category);
-        }
 
         // GET: Category/Delete/5
         public async Task<IActionResult> Delete(int? id)
